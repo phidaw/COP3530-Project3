3ce582894ffe25ae252f1b9e0a2b1cd6685ded99
nichnowicki <nichnowicki@gmail.com>
2025-04-22 02:16:25 +0000

    Updated Toolbox Class

--- src/GUI/Toolbox.h
+++ src/GUI/Toolbox.h
@@ -17,13 +17,77 @@ class Toolbox {
     friend class MazeStuff;
     friend void render();
     friend void gameLoop();
-    sf::Texture debugTexture;
-    sf::Sprite debugSprite;
+    sf::Texture OneUpTexture;
+    sf::Sprite OneUpSprite;
+    sf::Texture FiveUpTexture;
+    sf::Sprite FiveUpSprite;
+    sf::Texture TenUpTexture;
+    sf::Sprite TenUpSprite;
+    sf::Texture HundredUpTexture;
+    sf::Sprite HundredUpSprite;
+    sf::Texture OneDownTexture;
+    sf::Sprite OneDownSprite;
+    sf::Texture FiveDownTexture;
+    sf::Sprite FiveDownSprite;
+    sf::Texture TenDownTexture;
+    sf::Sprite TenDownSprite;
+    sf::Texture HundredDownTexture;
+    sf::Sprite HundredDownSprite;
+    sf::Texture mineNumsTexture;
+    std::pmr::vector<sf::Sprite> mineNumsSprite;
+    sf::Texture DijkstraTexture;
+    sf::Sprite DijkstraSprite;
+    sf::Texture AStarTexture;
+    sf::Sprite AStarSprite;
+    sf::Texture BFSTexture;
+    sf::Sprite BFSSprite;
+    sf::Texture ConfirmTexture;
+    sf::Sprite ConfirmSprite;
+    sf::Texture MazeTileTexture;
+    sf::Texture MazeTileTexture16;
+    sf::Texture MazeTileTexture8;
+    sf::Texture MiniMazeTileTexture;
+    sf::Texture MazePathTexture;
+    sf::Texture MiniMazePathTexture;
+    sf::Texture MazeSnakeBodyTexture;
+    sf::Texture MazeSnakeHeadTexture;
+
+
 
 public:
     sf::RenderWindow window;
-    Button* debugButton;
+    Button* OneUpButton;
+    Button* FiveUpButton;
+    Button* TenUpButton;
+    Button* HundredUpButton;
+    Button* OneDownButton;
+    Button* FiveDownButton;
+    Button* TenDownButton;
+    Button* HundredDownButton;
+    Button* DijkstraButton;
+    Button* AStarButton;
+    Button* BFSButton;
+    Button* ConfirmButton;
+    /*Tiles* MazeTile; //32, 16, 8, 3 bits for sizes? all done!
+    Tiles* MazePath;
+    Tiles* MazeSnakeBody;
+    Tiles* MazeSnakeHead;*/
+    int temp;
     static Toolbox &getInstance();
+    void changeMazeSize(int newSize);
+    void setMazeSprites(sf::Sprite newSprite);
+    int MazeSize;
+    int placeHolder;
+    std::vector<std::vector<sf::Sprite>> mazeTiles;
+    sf::Sprite MazeTileSprite;
+    sf::Sprite MazeTileSprite16;
+    sf::Sprite MazeTileSprite8;
+    sf::Sprite MiniMazeTileSprite;
+    sf::Sprite MazePathSprite;
+    sf::Sprite MiniMazePathSprite;
+    sf::Sprite MazeSnakeBodySprite;
+    sf::Sprite MazeSnakeHeadSprite;
+
 };
 
 
