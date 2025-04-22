8acba734e6fd0dd4b74fbe65d837c6f9e4dc14c4
nichnowicki <nichnowicki@gmail.com>
2025-04-22 02:11:29 +0000

    Deleted unused Tile class

--- src/GUI/Tiles.cpp
+++
@@ -1,9 +0,0 @@
-#include "Tiles.h"
-#include "Toolbox.h"
-#include <iostream>
-#include <SFML/Graphics.hpp>
-#include <functional>
-#include <SFML/Graphics.hpp>
-#include "MazeStuff.h"
-#include "Button.h"
-//Look at Tiles.h
