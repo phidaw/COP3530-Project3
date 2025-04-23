256788fbb616e7bbf79c255179a280a3cb85fc43
Nicholas0202 <60819000+Nicholas0202@users.noreply.github.com>
2025-04-22 23:18:51 -0400

    Update Toolbox.cpp

--- src/GUI/Toolbox.cpp
+++ src/GUI/Toolbox.cpp
@@ -2,18 +2,20 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <functional>
-#include <SFML/Graphics.hpp>
-#include "MazeStuff.h"
-#include "Button.h"
+#include <map>
 #include <string>
+#include <vector>
+#include "Button.h"
+#include "MazeStuff.h"
+#include "../../Maze/Maze.h"
 #include "../../Traversal/Agents/AStarAgent.h"
-#include "../../Traversal/BFSAgent.h"
-#include "../../Traversal/DijkstraAgent.h"
+#include "../../Traversal/Agents/BFSAgent.h"
+#include "../../Traversal/Agents/DijkstraAgent.h"
 
 Toolbox* Toolbox::instance = nullptr;
 
 Toolbox::Toolbox() {
-    window.create(sf::VideoMode(1520, 951), "Maze Maestro"); //Biggest size that looks ok is 1500 * 900
+    window.create(sf::VideoMode(1520, 951), "Maze Maestro");
     MazeSize = 1;
     placeHolder = -1;
     temp = 0;
@@ -25,78 +27,65 @@ Toolbox::Toolbox() {
     FiveDownButton = new Button(sf::Vector2f(1160, 140), [this]{fiveDown();});
     TenDownButton = new Button(sf::Vector2f(1120, 140), [this]{tenDown();});
     HundredDownButton = new Button(sf::Vector2f(1080, 140), [this]{hundredDown();});
-    DijkstraButton = new Button(sf::Vector2f(1200, 300), [this]{PlaceHolder();});
-    AStarButton = new Button(sf::Vector2f(1200, 450), [this]{PlaceHolder();});
-    BFSButton = new Button(sf::Vector2f(1200, 600), [this]{PlaceHolder();});
+    DijkstraButton = new Button(sf::Vector2f(1200, 300), [this]{addDijkstraAgent();});
+    AStarButton = new Button(sf::Vector2f(1200, 450), [this]{addAStarAgent();});
+    BFSButton = new Button(sf::Vector2f(1200, 600), [this]{addBFSAgent();});
     ConfirmButton = new Button(sf::Vector2f(1260, 180), [this]{confirmation();});
-    ComplicatedConfirmButton = new Button(sf::Vector2f(1240, 800), [this]{PlaceHolder();});
-    ComplicateCancelButton = new Button(sf::Vector2f(1280, 800), [this]{PlaceHolder();});
     mazeTiles = std::vector(MazeSize, std::vector<sf::Sprite>(MazeSize, sf::Sprite{}));
     mazeTilesTypes.resize(MazeSize, std::vector<std::string>(MazeSize, std::string()));
-
-
-
+    totalAgentPaths = std::map<std::string, std::vector<Cell*>>();
     if (!OneUpTexture.loadFromFile("resources/images/1Up.png")) {
         std::cerr << "Failed to load 1Up texture";
-    }
-    else { //This creates the sprite of the button, and places it in pos^, and assigns the sprite to the appropriate button
+    } else {
         OneUpSprite.setTexture(OneUpTexture);
         OneUpSprite.setPosition(OneUpButton->getPosition());
         OneUpButton->setSprite(&OneUpSprite);
     }
     if (!FiveUpTexture.loadFromFile("resources/images/5Up.png")) {
         std::cerr << "Failed to load 5Up texture";
-    }
-    else {
+    } else {
         FiveUpSprite.setTexture(FiveUpTexture);
         FiveUpSprite.setPosition(FiveUpButton->getPosition());
         FiveUpButton->setSprite(&FiveUpSprite);
     }
     if (!TenUpTexture.loadFromFile("resources/images/10Up.png")) {
         std::cerr << "Failed to load 10Up texture";
-    }
-    else {
+    } else {
         TenUpSprite.setTexture(TenUpTexture);
         TenUpSprite.setPosition(TenUpButton->getPosition());
         TenUpButton->setSprite(&TenUpSprite);
     }
     if (!HundredUpTexture.loadFromFile("resources/images/100Up.png")) {
         std::cerr << "Failed to load 100Up texture";
-    }
-    else {
+    } else {
         HundredUpSprite.setTexture(HundredUpTexture);
         HundredUpSprite.setPosition(HundredUpButton->getPosition());
         HundredUpButton->setSprite(&HundredUpSprite);
     }
-
     if (!OneDownTexture.loadFromFile("resources/images/1Down.png")) {
         std::cerr << "Failed to load 1Down texture";
-    }
-    else {
+    } else {
         OneDownSprite.setTexture(OneDownTexture);
         OneDownSprite.setPosition(OneDownButton->getPosition());
         OneDownButton->setSprite(&OneDownSprite);
     }
     if (!FiveDownTexture.loadFromFile("resources/images/5Down.png")) {
         std::cerr << "Failed to load 5Down texture";
-    }
-    else {
+    } else {
         FiveDownSprite.setTexture(FiveDownTexture);
         FiveDownSprite.setPosition(FiveDownButton->getPosition());
         FiveDownButton->setSprite(&FiveDownSprite);
     }
     if (!TenDownTexture.loadFromFile("resources/images/10Down.png")) {
         std::cerr << "Failed to load 10Down texture";
-    }
-    else {
+    } else {
         TenDownSprite.setTexture(TenDownTexture);
         TenDownSprite.setPosition(TenDownButton->getPosition());
         TenDownButton->setSprite(&TenDownSprite);
     }
     if (!HundredDownTexture.loadFromFile("resources/images/100Down.png")) {
         std::cerr << "Failed to load 100Down texture";
-    }
-    else {
+    } else {
         HundredDownSprite.setTexture(HundredDownTexture);
         HundredDownSprite.setPosition(HundredDownButton->getPosition());
         HundredDownButton->setSprite(&HundredDownSprite);
@@ -111,141 +100,134 @@ Toolbox::Toolbox() {
         sf::Sprite tempSprite(mineNumsTexture, digitRect);
         mineNumsSprite.push_back(tempSprite);
     }
-
     if (!DijkstraTexture.loadFromFile("resources/images/DijkstraButton.png")) {
         std::cerr << "Failed to load Dijkstra texture";
-    }
-    else {
+    } else {
         DijkstraSprite.setTexture(DijkstraTexture);
         DijkstraSprite.setPosition(DijkstraButton->getPosition());
         DijkstraButton->setSprite(&DijkstraSprite);
     }
     if (!AStarTexture.loadFromFile("resources/images/AStarButton.png")) {
         std::cerr << "Failed to load AStar texture";
-    }
-    else {
+    } else {
         AStarSprite.setTexture(AStarTexture);
         AStarSprite.setPosition(AStarButton->getPosition());
         AStarButton->setSprite(&AStarSprite);
     }
     if (!BFSTexture.loadFromFile("resources/images/BFSButton.png")) {
         std::cerr << "Failed to load BFS texture";
-    }
-    else {
+    } else {
         BFSSprite.setTexture(BFSTexture);
         BFSSprite.setPosition(BFSButton->getPosition());
         BFSButton->setSprite(&BFSSprite);
     }
     if (!ConfirmTexture.loadFromFile("resources/images/ConfirmButton.png")) {
-        std::cerr << "Failed to load Confirm texture";
-    }
-    else {
+        ška: std::cerr << "Failed to load Confirm texture";
+    } else {
         ConfirmSprite.setTexture(ConfirmTexture);
-        ComplicatedConfirmSprite.setTexture(ConfirmTexture);
         ConfirmSprite.setPosition(ConfirmButton->getPosition());
-        ComplicatedConfirmSprite.setPosition(ComplicatedConfirmButton->getPosition());
         ConfirmButton->setSprite(&ConfirmSprite);
-        ComplicatedConfirmButton->setSprite(&ComplicatedConfirmSprite);
     }
     if (!MazeTileTexture.loadFromFile("resources/images/MazeTile.png")) {
         std::cerr << "Failed to load Maze Tile texture";
-    }
-    else {
+    } else {
         MazeTileSprite.setTexture(MazeTileTexture);
     }
     if (!MazePathTexture.loadFromFile("resources/images/MazeTilePath.png")) {
         std::cerr << "Failed to load Maze Path texture";
-    }
-    else {
+    } else {
         MazePathSprite.setTexture(MazePathTexture);
     }
     if (!MazeSnakeBodyTexture.loadFromFile("resources/images/MazeTileSnakeBody.png")) {
         std::cerr << "Failed to load Maze Snake Body texture";
-    }
-    else {
+    } else {
         MazeSnakeBodySprite.setTexture(MazeSnakeBodyTexture);
     }
     if (!MazeSnakeHeadTexture.loadFromFile("resources/images/MazeTileSnakeHead.png")) {
         std::cerr << "Failed to load Maze Snake Head texture";
-    }
-    else {
+    } else {
         MazeSnakeHeadSprite.setTexture(MazeSnakeHeadTexture);
     }
     if (!MazeTileTexture16.loadFromFile("resources/images/MazeTile16.png")) {
         std::cerr << "Failed to load Maze Tile 16 bit texture";
-    }
-    else {
+    } else {
         MazeTileSprite16.setTexture(MazeTileTexture16);
     }
     if (!MazeTileTexture8.loadFromFile("resources/images/MazeTile8.png")) {
         std::cerr << "Failed to load Maze Tile 8 bit texture";
-    }
-    else {
+    } else {
         MazeTileSprite8.setTexture(MazeTileTexture8);
     }
     if (!MiniMazeTileTexture.loadFromFile("resources/images/MiniMazeTile.png")) {
         std::cerr << "Failed to load Mini Maze Tile texture";
-    }
-    else {
+    } else {
         MiniMazeTileSprite.setTexture(MiniMazeTileTexture);
     }
     if (!MiniMazePathTexture.loadFromFile("resources/images/MiniMazeTilePath.png")) {
         std::cerr << "Failed to load Mini Maze Path texture";
-    }
-    else {
+    } else {
         MiniMazePathSprite.setTexture(MiniMazePathTexture);
     }
     if (!MazePathTexture16.loadFromFile("resources/images/MazeTilePath16.png")) {
         std::cerr << "Failed to load Maze Path 16 bit texture";
-    }
-    else {
+    } else {
         MazePathSprite16.setTexture(MazePathTexture16);
     }
     if (!MazePathTexture8.loadFromFile("resources/images/MazeTilePath8.png")) {
         std::cerr << "Failed to load Maze Path 8 bit texture";
-    }
-    else {
+    } else {
         MazePathSprite8.setTexture(MazePathTexture8);
     }
     if (!MazeSnakeBodyTexture16.loadFromFile("resources/images/MazeTileSnakeBody16.png")) {
         std::cerr << "Failed to load Maze Snake Body 16 bit texture";
-    }
-    else {
+    } else {
         MazeSnakeBodySprite16.setTexture(MazeSnakeBodyTexture16);
     }
     if (!MazeSnakeBodyTexture8.loadFromFile("resources/images/MazeTileSnakeBody8.png")) {
         std::cerr << "Failed to load Maze Snake Body 8 bit texture";
-    }
-    else {
+    } else {
         MazeSnakeBodySprite8.setTexture(MazeSnakeBodyTexture8);
     }
     if (!MiniMazeSnakeTexture.loadFromFile("resources/images/MiniMazeTileSnake.png")) {
         std::cerr << "Failed to load Mini Maze Snake texture";
-    }
-    else {
+    } else {
         MiniMazeSnakeSprite.setTexture(MiniMazeSnakeTexture);
     }
     if (!MazeSnakeHeadTexture16.loadFromFile("resources/images/MazeTileSnakeHead16.png")) {
         std::cerr << "Failed to load Maze Snake Head 16 bit texture";
-    }
-    else {
+    } else {
         MazeSnakeHeadSprite16.setTexture(MazeSnakeHeadTexture16);
     }
-    if (!CancelTexture.loadFromFile("images/CancelButton.png")) {
-        std::cerr << "Failed to load Cancel texture";
-    }
-    else {
-        CancelSprite.setTexture(CancelTexture);
-        CancelSprite.setPosition(ComplicateCancelButton->getPosition());
-        ComplicateCancelButton->setSprite(&CancelSprite);
-    }
 }
+
+void Toolbox::addDijkstraAgent() {
+    static int dijkstraCount = 0;
+    DijkstraAgent* agent = new DijkstraAgent("Dijkstra" + std::to_string(++dijkstraCount));
+    // Assuming there's a global or accessible AgentManager instance
+    // This requires AgentManager to be accessible, e.g., via a singleton or passed instance
+    // For now, assuming a hypothetical global agentManager
+    agentManager.AddAgent(*agent);
+}
+
+void Toolbox::addAStarAgent() {
+    static int aStarCount = 0;
+    AStarAgent* agent = new AStarAgent("AStar" + std::to_string(++aStarCount));
+    agentManager.AddAgent(*agent);
+}
+
+void Toolbox::addBFSAgent() {
+    static int bfsCount = 0;
+    BFSAgent* agent = new BFSAgent("BFS" + std::to_string(++bfsCount));
+    agentManager.AddAgent(*agent);
+}
+
 Toolbox &Toolbox::getInstance() {
     if (instance == nullptr) {
         instance = new Toolbox();
     }
     return *instance;
 }
+
 void Toolbox::changeMazeSize(int newSize) {
     temp = newSize;
     mazeTiles.resize(newSize);
@@ -254,18 +236,41 @@ void Toolbox::changeMazeSize(int newSize) {
     }
     mazeTilesTypes.resize(newSize);
 }
+
 void Toolbox::setMazeSprites(sf::Sprite newSprite) {
     for (auto& row : mazeTiles) {
         for (auto& sprites : row) {
             sprites = newSprite;
         }
     }
-    /*if (MazeSize > 120) {
-        mazeTiles[50][100] = MiniMazePathSprite;
-        mazeTiles[50][101] = MiniMazePathSprite;
-        mazeTiles[50][102] = MiniMazePathSprite;
-        mazeTiles[50][103] = MiniMazePathSprite;
-        mazeTiles[51][101] = MiniMazePathSprite;
-        mazeTiles[52][101] = MiniMazePathSprite;
-    }*/
+}
+
+std::string Toolbox::getMazeTilesTypesString() const {
+    std::string result;
+    for (const auto& row : mazeTilesTypes) {
+        for (const auto& tileType : row) {
+            result += tileType.empty() ? "empty" : tileType;
+            result += " ";
+        }
+        result += "\n";
+    }
+    return result;
+}
+
+void Toolbox::appendAgentPath(const std::string& agentName, const std::vector<Cell*>& path) {
+    auto& totalPath = totalAgentPaths[agentName];
+    if (!path.empty()) {
+        size_t startIndex = (totalPath.empty() || path[0] != totalPath.back()) ? 0 : 1;
+        totalPath.insert(totalPath.end(), path.begin() + startIndex, path.end());
+    }
+}
+
+const std::vector<Cell*>& Toolbox::getAgentPath(const std::string& agentName) const {
+    static const std::vector<Cell*> emptyPath;
+    auto it = totalAgentPaths.find(agentName);
+    return it != totalAgentPaths.end() ? it->second : emptyPath;
+}
+
+void Toolbox::clearAgentPaths() {
+    totalAgentPaths.clear();
 }
