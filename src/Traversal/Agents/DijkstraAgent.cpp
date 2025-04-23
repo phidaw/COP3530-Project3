160a61ca0d30963e4036ebe3667340fc88f3d6ca
Nicholas0202 <60819000+Nicholas0202@users.noreply.github.com>
2025-04-22 20:37:45 -0400

    Update DijkstraAgent.cpp

--- src/Traversal/Agents/DijkstraAgent.cpp
+++ src/Traversal/Agents/DijkstraAgent.cpp
@@ -5,10 +5,21 @@
 #include "DijkstraAgent.h"
 
 void DijkstraAgent::UpdateVisuals()
-{
-    // todo -- update GUI here --
-    // use this->currCell
-}
+/*{
+    Toolbox& toolbox = Toolbox::getInstance();
+    int tileSize = 32;
+    if (toolbox.MazeSize > 29 && toolbox.MazeSize <= 59) tileSize = 16;
+    else if (toolbox.MazeSize > 59 && toolbox.MazeSize <= 118) tileSize = 8;
+    else if (toolbox.MazeSize > 118 && toolbox.MazeSize <= 317) tileSize = 3;
+
+    headSprite->setPosition(currCell->pos->x * tileSize, currCell->pos->y * tileSize);
+    toolbox.window.draw(*headSprite);
+
+    if (prevCell) {
+        pathSprites[0].setPosition(prevCell->pos->x * tileSize, prevCell->pos->y * tileSize);
+        toolbox.window.draw(pathSprites[0]);
+    }
+}*/
 
 void DijkstraAgent::UpdateTimer()
 {
